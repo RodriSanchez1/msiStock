e965b672cc34635825962f5c5a9d9ea6076fa04d
Florencia Ulloque <m.floo95a@gmail.com>
2021-10-14 11:53:05 -0300

    Marca y rubro

---
+++ src/Modelos/Marca.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author Flor
+ */
+public class Marca {
+    
+    private int idMarca;
+    private String nombre;
+
+    public int getIdMarca() {
+        return idMarca;
+    }
+
+    public void setIdMarca(int idMarca) {
+        this.idMarca = idMarca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Marca(int idMarca, String nombre) {
+        this.idMarca = idMarca;
+        this.nombre = nombre;
+    }
+
+    public Marca() {
+    }
+
+    @Override
+    public String toString() {
+        return "Marca{" + "idMarca=" + idMarca + ", nombre=" + nombre + '}';
+    }
+    
+    
+}
