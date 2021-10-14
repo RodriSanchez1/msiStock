eee0e64f28bde36fb6b46b3ecebf44c7da8046de
AlexisAllamano <90708460+AlexisAllamano@users.noreply.github.com>
2021-10-14 11:31:10 -0300

    Creando Clase Depósito - Prueba

---
+++ src/Modelos/Depósito.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author Alex
+ */
+public class Depósito {
+    private int numeroDeposito;
+
+    public Depósito(int numeroDeposito) {
+        this.numeroDeposito = numeroDeposito;
+    }
+
+    public Depósito() {
+    }
+    
+    
+
+    public int getNumeroDeposito() {
+        return numeroDeposito;
+    }
+
+    public void setNumeroDeposito(int numeroDeposito) {
+        this.numeroDeposito = numeroDeposito;
+    }
+    
+    
+    
+}
